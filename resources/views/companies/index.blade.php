8e7ec26d5e39ffa8fd820c3f53393e29cdf21096
bwissal <baazizwissal13@gmail.com>
2024-01-31 11:13:19 +0100

    add company view

---
+++ resources/views/companies/index.blade.php
@@ -0,0 +1,77 @@
+
+@extends('Companies.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Test view</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('companies.create') }}"> Create New Company</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Domain</th>
+            <th>Contact</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($companies as $company)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $company->name }}</td>
+            <td>{{ $company->domain }}</td>
+            <td>{{ $company->contact }}</td>
+            <td>
+                <form action="{{ route('companies.destroy',$company->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('companies.show',$company->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('companies.edit',$company->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $companies->links() !!}
+      
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Company</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('companies.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+
+@endsection
