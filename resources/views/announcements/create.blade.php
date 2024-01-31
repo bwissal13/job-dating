3ff871ae7d4ec56bc54ea7715881e323c8dafc1d
bwissal <baazizwissal13@gmail.com>
2024-01-31 17:01:31 +0100

    annoucement create

---
+++ resources/views/announcements/create.blade.php
@@ -0,0 +1,73 @@
+@extends('announcements.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New announcement</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('announcements.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('announcements.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Title:</strong>
+                <input type="text" name="title" class="form-control" placeholder="Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                <input type="text" name="description" class="form-control" placeholder="Domain">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Date:</strong>
+                <input type="text" name="date" class="form-control" placeholder="Contact">
+            </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <label for="company_id">Company</label>
+                <select name="company_id" class="form-control">
+                    @foreach($companies as $company)
+                        <option value="{{ $company->id }}">{{ $company->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+            </div>
+        </div>
+        
+        
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
