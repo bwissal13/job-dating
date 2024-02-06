8ab335cab3495f53e797ced1cb5b03d236002539
bwissal <baazizwissal13@gmail.com>
2024-02-06 10:34:19 +0100

    add skill

---
+++ app/Http/Controllers/SkillController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SkillRequest;
+use App\Models\Skill;
+use App\Http\Requests\StoreSkillRequest;
+use App\Http\Requests\UpdateSkillRequest;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $skills = Skill::all();
+        return view('skills.index', compact('skills'))
+                    ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('skills.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(SkillRequest $request)
+    {
+        
+        Skill::create($request->validated());
+
+        return redirect()->route('skills.create')
+                            ->with('success', 'Skill created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Skill $skill)
+    {
+        return view('skills.show', compact('skill'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Skill $skill)
+    {
+       
+        return view('skills.edit', compact('skill'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(SkillRequest $request, Skill $skill)
+    {
+        $skill->update($request->validated());
+        return redirect()->route('skills.index')
+                            ->with('success', 'Skill updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Skill $skill)
+    {
+        $skill->delete();
+        return redirect()->route('skills.index')
+        ->with('success', 'Skill deleted successfully');
+    }
+}
---
+++ app/Models/Skill.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Skill extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'skills_users');
+    }
+
+    public function announcements()
+    {
+        return $this->belongsToMany(Announcements::class, 'skills_announcements');
+    }
+}
